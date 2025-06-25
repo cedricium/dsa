0ec86db9f518564c0a57559fdb89d0adb65b8812
Cedric Amaya <amaya.cedric@gmail.com>
2025-06-25 13:15:03 -0700

    docs: add missing problem description

--- src/problems/chapter-01/1.4-palindrome-permutation/index.ts
+++ src/problems/chapter-01/1.4-palindrome-permutation/index.ts
@@ -4,7 +4,10 @@ export const problem = {
 };
 
 /**
- *
+ * Given a string, write a function to check if it is a permutation of a
+ * palindrome. A palindrome is a word or phrase that is the same forwards and
+ * backwards. A permutation is a rearrangement of letters. The palindrome does
+ * not need to be limited to just dictionary words.
  */
 function palindromePermutation(str: string): boolean {
   const charMap = new Map<string, number>();
